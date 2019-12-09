fc7ed96c0e7b464297e63b33988220e60bb0b8f4
Hong Jik Lee <hongjik92@daum.net>
2019-12-09 16:49:19 -0500

    remove unnecessary stuff

--- content.js
+++ content.js
@@ -6,20 +6,13 @@ for (elt of paragraphs) {
   elt.style['background-color'] = '#FF00FF';
 }
 
-// The ID of the extension we want to talk to.
-var editorExtensionId = "iafdhjkhomfodbccifgginllanlfphof";
-
 window.addEventListener("message", function(event) {
-    // We only accept messages from this window to itself [i.e. not from any iframes]
     if (event.source != window)
       return;
   
-    if (event.data.type && (event.data.type == "FROM_PAGE_TO_CONTENT_SCRIPT")) {    
-        
-        this.console.log(event.data.text)
+    if (event.data.type && (event.data.type == "FROM_PAGE_TO_CONTENT_SCRIPT")) {            
         eval(event.data.text)
-        //chrome.runtime.sendMessage(event.data); // broadcasts it to rest of extension, or could just broadcast event.data.payload...
-    } // else ignore messages seemingly not sent to yourself
+    } 
 }, false);
 
 console.log("hello")
